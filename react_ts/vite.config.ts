146f8b727f7ef0b89b6557b53b062052d763e7e4
Marquez <17600340273@163.com>
2024-08-31 15:04:43 +0800

    update html

--- react_ts/vite.config.ts
+++ react_ts/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'alancumberbatch.github.io',  // 这里替换为你的 GitHub 仓库名称
   plugins: [react()],
 })
